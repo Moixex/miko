4e34cbd0cae6f33f405604e907b571ab9f0fe0db
kaisvl <plusgratose@gmail.com>
2025-11-12 18:10:13 -0500

    Update EventHandler.ts

--- src/handlers/EventHandler.ts
+++ src/handlers/EventHandler.ts
@@ -1,26 +1,31 @@
 import { Event } from "@structures/core";
 import path from "path";
-import { client } from "@root";
 import { FileManager } from "@utils/managers";
+import { Bot } from "@core/Bot";
+
 const eventsPath = path.join(__dirname, "../events");
 
 export abstract class EventHandler {
-	public static async registerEvents() {
-		const eventsFiles = FileManager.getJSFiles(eventsPath);
-		for (const file of eventsFiles) {
-			const event = await import(file) as Event<any>;
-			if ("run" in event) {
-				const run = (...args: any[]) => event.run(client, ...args)
-				client.events.set(event.name, run);
-				if (event.rest) {
-					if (event.once) client.rest.once(event.name, run)
-					else client.rest.on(event.name, run)
-				} else {
-					if (event.once) client.once(event.name, run)
-					else client.on(event.name, run)
-				}
-			}
-		}
-	}
+  public static async registerEvents(client: Bot) {
+    const eventFiles = FileManager.getJSFiles(eventsPath);
+
+    for (const file of eventFiles) {
+      const event = await import(file) as Event<any>;
+
+      if ("run" in event) {
+        const run = (...args: any[]) => event.run(client, ...args);
+
+        client.events.set(event.name, run);
+
+        if (event.rest) {
+          if (event.once) client.rest.once(event.name, run);
+          else client.rest.on(event.name, run);
+        } else {
+          if (event.once) client.once(event.name, run);
+          else client.on(event.name, run);
+        }
+      }
+    }
+  }
 }
 
